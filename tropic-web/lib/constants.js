f9bcb0fb4fdaeb265d7794d1ecf29952085550a9
utf <alexganose@googlemail.com>
2025-08-29 15:58:50 +0100

    API fix

--- tropic-web/lib/constants.js
+++ tropic-web/lib/constants.js
@@ -1,2 +1,2 @@
 export const API_ENDPOINT =
-  process.env.NEXT_TROPIC_API_ENDPOINT || process.env.TROPIC_API_ENDPOINT || "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_TROPIC_API_ENDPOINT || process.env.TROPIC_API_ENDPOINT || "http://127.0.0.1:8000";
